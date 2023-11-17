2d0f618f9bbed643a3a029b3019370bd6a32f811
gitxfu <76028008+gitxfu@users.noreply.github.com>
2023-11-16 19:58:42 -0500

    implement search sort and filter category

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,10 @@ const Home = () => {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [searchInput, setSearchInput] = useState('');
+    const [sortCriteria, setSortCriteria] = useState('created_at'); // 'created_at' or 'like_count'
+    const [filterCategory, setFilterCategory] = useState(''); // '', 'question', 'blog'
+
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -37,23 +41,70 @@ const Home = () => {
         fetchPosts();
     }, []);  // runs once when component mounts
 
+    const getSelectedPosts = () => {
+        let selectedPosts = posts;
+
+        if (filterCategory) {
+            selectedPosts = selectedPosts.filter(item => item.category === filterCategory)
+        }
+
+        if (sortCriteria === 'like_count') {
+            selectedPosts.sort((a, b) => b.like_count - a.like_count);
+        } else {
+            selectedPosts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+        }
+
+        if (searchInput) {
+            selectedPosts = selectedPosts.filter(item =>
+                item.title.toLowerCase().includes(searchInput.toLowerCase()));
+        }
+
+        return selectedPosts;
+    };
+
+    const seletedPosts = getSelectedPosts();
+
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
     if (posts.length === 0) return <h2>{'No Post Yet 😞'}</h2>
 
+
+
     return (
-        <div className="card-container">
-            {
-                posts.map((post) =>
+        <>
+            <div>
+                <input
+                    name="search"
+                    type="text"
+                    placeholder="Search..."
+                    onChange={(e) => setSearchInput(e.target.value)}
+                />
+                <select onChange={(e) => setSortCriteria(e.target.value)}>
+                    <option value="created_at">Sort by Time</option>
+                    <option value="like_count">Sort by Likes</option>
+                </select>
+                <select onChange={(e) => setFilterCategory(e.target.value)}>
+                    <option value="">All Categories</option>
+                    <option value="Question">Questions</option>
+                    <option value="Blog">Blogs</option>
+                </select>
+            </div>
+            <div className="card-container">
+                {seletedPosts && seletedPosts.length > 0 ? seletedPosts.map((post) => (
                     <Card key={post.id}
                         id={post.id}
+                        category={post.category}
                         timeAgo={post.timeAgo}
                         title={post.title}
                         image={post.image}
                         likeCount={post.like_count}
                     />)
-            }
-        </div>
+                ) : <h1> No Post Found</h1>
+
+                }
+            </div>
+        </>
+
     );
 
 
